a4d29c0d3f69b71266817983d1e842946b53a07b
shaibusterling <pattiminka@gmail.com>
2021-11-24 14:04:25 +0100

    Adding method generate_code which generates 7 characters for a given original_url.. generate_code method will run and set a short_url of a link before saving into the DB

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,24 @@
 class Link < ApplicationRecord
+    require "digest/sha2"
 
     validates :original_url, presence: true, allow_blank: false
+
+    before_create  :generate_code
+
+   
+
+
+
+
+
+
+    
+    def generate_code
+    self.short_url = Digest::SHA256.hexdigest(self.original_url)[0..6]
+    end
+
+
+
+
 
 end
--- spec/features/creating_link_spec.rb
+++ spec/features/creating_link_spec.rb
@@ -21,4 +21,72 @@ RSpec.describe type: :system do
 
 
 
+    it "generates 7 characters for a given original url" do
+
+        link = Link.new(
+            original_url: "https://www.rubyguides.com/2015/03/ruby-random/"
+        )
+
+        expect(link.generate_code.length).to eq(7)
+    end
+
+
+
+  
+#   it "generates characters for an empty original url but dose not save" do
+
+#         link = Link.new(
+#             original_url: " ",
+#             short_url: " "
+#         )
+
+#        link.short_url = link.generate_code 
+
+#         expect(link.save).to eq(false)
+#     end
+
+    # it "generates 7 different characters for each given original url" do
+
+    #     link_one = Link.new(
+    #         original_url: "https://www.example.com/books",
+    #         short_url: ""
+    #     )
+
+    #     link_two = Link.new(
+    #         original_url: "https://www.bachelorsportal.com/search/bachelor",
+    #         short_url: ""
+    #     )
+
+    #     expect(link_one.generate_code).to_not eq(link_two.generate_code)
+    # end
+
+
+
+    # it "has valid original url" do
+    #     visit new_link_path 
+    #     fill_in "Original url", with: "https://www.bachelorsportal.com/search/bachelor"
+    #     click_button "Create Link"
+    #     expect(page).to have_content("localhost:3000/")
+    # end
+
+  
+
+    
+    # it "increases clicked by 1 " do
+
+    #     link = Link.new(
+    #         original_url: "https://www.rubyguides.com/2015/03/ruby-random/",
+    #         short_url: "1234567"
+    #     )
+
+    #     link.set_click 
+
+    #     expect(link.clicked).to eq(1)
+    # end
+
+
+
+
+
+
 end
