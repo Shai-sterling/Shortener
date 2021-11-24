d0c44975864b5b3bdec9256f0382932393a37a96
shaibusterling <pattiminka@gmail.com>
2021-11-24 16:55:55 +0100

    Adding set_click method which increases the value of clicked of a link when visited by 1.

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,11 +10,15 @@ class Link < ApplicationRecord
 
 
 
+    def set_click
+        self.increment!(:clicked, 1)
+     end
+
 
 
     
     def generate_code
-    self.short_url = Digest::SHA256.hexdigest(self.original_url)[0..6]
+        self.short_url = Digest::SHA256.hexdigest(self.original_url)[0..6]
     end
 
 
