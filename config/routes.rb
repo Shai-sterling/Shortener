c21e2b024c1ed7fe7face89d4df99999d28f20cd
shaibusterling <pattiminka@gmail.com>
2021-11-24 13:21:18 +0100

    Adding links resources routes and mapping root path to links new path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-get "welcome", to: "welcome#index"
-root "welcome#index"
+  get "welcome", to: "welcome#index"
 
-resources :links
+
+  root "links#new"
+
+  resources :links
+
+
+
+
+
+
 end
